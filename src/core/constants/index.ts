ab63fe8507f685b516bf4c95927c3b36fbeb4204
Alessandro Rios <drios28@outlook.es>
2024-07-11 23:52:36 -0500

    update: add husky, add robots and sitemap.ts, add APP_URL

--- src/core/constants/index.ts
+++ src/core/constants/index.ts
@@ -2,6 +2,9 @@
 export const APP_INDEX_TITLE = 'Template | Next.js'
 export const APP_INDEX_DESCRIPTION = 'Template Next.js with TailwindCSS, TypeScript, ESLint and Husky'
 
+/* APP INFO */
+export const APP_URL = process.env.NODE_ENV === 'production' ? '' : 'http://localhost:3000'
+
 /* AUTHOR */
 export const APP_AUTHOR = 'Alessandro Rios'
 export const APP_AUTHOR_WEBSITE = 'https://alessandrorios.com'
